0ba9d5f2ea1c75c4d0e6b371d3b5af7283f31e9b
Fasakin Henry <fasakinhenry@gmail.com>
2024-03-08 01:09:25 +0100

    Add learnings for the sentence manipulation

---
+++ Python/hello_world/print_sentence.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This code will print sentence"object-oriented programming with Python"
+from the str variable by slicing"""
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = f"{str[39:66]}{str[106:112]}{str[:6]}"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str)
