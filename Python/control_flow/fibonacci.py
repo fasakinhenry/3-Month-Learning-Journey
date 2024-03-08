5dff53cb2951b1675603fb2df9154cb4b67e1bf8
Fasakin Henry <fasakinhenry@gmail.com>
2024-03-08 13:56:42 +0100

    Refactor code to follow PEP8 pycodestyle format

--- Python/control_flow/fibonacci.py
+++ Python/control_flow/fibonacci.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # The sum of the two elements define the next
-a,b = 0, 1
+a, b = 0, 1
 while a < 10:
-	print(a)
-	a , b = b, a+b
+    print(a)
+    a, b = b, a + b
--- Python/control_flow/find_prime_numbers.py
+++ Python/control_flow/find_prime_numbers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for n in range(2, 18):
-	for x in range(2, n):
-		if n % x == 0:
-			print(n, 'equals', x, '*', n//x)
-			break
-	else:
-		# loop fell through without finding a factor
-		print(n, 'is a prime number')
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
