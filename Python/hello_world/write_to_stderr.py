bb1d30b5336f238a9d12d17f294902010c3a1cd1
Fasakin Henry <fasakinhenry@gmail.com>
2024-03-08 01:48:33 +0100

    Add learnings for writing to standard error

---
+++ Python/hello_world/write_to_stderr.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" This writes "and that piece of art is useful - Dora Korpar, 2015-10-19"
+to standard error(stderr)"""
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
