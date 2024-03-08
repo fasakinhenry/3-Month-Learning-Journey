ee79a83ed32e85e18a0af6ff54118fc73cc8677a
Fasakin Henry <fasakinhenry@gmail.com>
2024-03-08 22:46:22 +0100

    Add tests for negative numbers

--- Python/control_flow/check_last_digit.py
+++ Python/control_flow/check_last_digit.py
@@ -3,7 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 """ Checks if the last digit of number is
 greater than 5, equals 0 or less than 6 and not 0"""
-last_digit = number % 10
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
 match last_digit:
     case 0:
         print(f"Last digit of {number} is {last_digit} and is 0")
