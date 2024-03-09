0b9357fa9581410dcc8326a2ed4b48dc77b25811
Fasakin Henry <fasakinhenry@gmail.com>
2024-03-09 13:29:24 +0100

    Add documentation for print_combination2.py

--- Python/control_flow/print_combination2.py
+++ Python/control_flow/print_combination2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# Print the different combination of 2digit numbers
+# The two digits must be different
 for i in range(10):
     for j in range(i + 1, 10):
         print(f"{i}{j}", end=", "
