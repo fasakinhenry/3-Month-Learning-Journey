1db3b66954b150fed3b39a5e3536834ce021903f
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-05 23:31:13 +0100

    Add function to safely print list without indexerror

--- Python/exceptions/safe_print_list.py
+++ Python/exceptions/safe_print_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-# Safe printing of values
+# function that prints x elements of a list.
+def safe_print_list(my_list=[], x=0):
+        count = 0
+        try:
+            for element in range(x):
+                print(my_list[element], end="")
+                count += 1
+        except IndexError:
+            # Handle case where index is out of range
+            pass
+        # Print newline if all elements are printed
+        print()
+        return count
