7377b0f4c50b965dc74fa82d95175b85cadedc41
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 20:25:00 +0100

    Add exception for wrong data type in class

---
+++ Python/classes/square_optional.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class square"""
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0):
+        """Initialize a new square
+        Args:
+            size (int): The size of the new square"""
+        # check if size is an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
