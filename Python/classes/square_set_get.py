f60024356c4b640fe84a159b2489aa4944238593
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 22:50:56 +0100

    Add code to add getter and setters in python class

---
+++ Python/classes/square_set_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# square_set_get
+"""Define a class square"""
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0):
+        """Initialize a new square
+        Args:
+            size (int): The size of the new square"""
+        self.size = size
+        
+
+    @property
+    def size(self):
+        """Retrives the value of the private size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the value of the private size attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size ** 2)
