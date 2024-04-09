f720236f5e5b40c9ca0e29874b60d483b7b43535
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 20:15:10 +0100

    Add code to practice learning private attributes in classes

---
+++ Python/classes/square_private_attr.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class square"""
+class Square:
+    """Represent a square"""
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
+            size (int): The size of the new square"""
+        self.__size = size
