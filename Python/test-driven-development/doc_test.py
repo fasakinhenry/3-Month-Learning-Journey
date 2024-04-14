a4d33eb6ed1a4ff1898170b4487074c59389c42d
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-14 23:57:59 +0100

    Add docs for the test file

--- Python/test-driven-development/doc_test.py
+++ Python/test-driven-development/doc_test.py
@@ -7,7 +7,7 @@ The example module supplies one function, factorial().  For example,
 >>> factorial(5)
 120
 """
-# add unwanted doc
+print("hello")
 def factorial(n):
     """Return the factorial of n, an exact integer >= 0.
 
