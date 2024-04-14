2334dc77c6a856d8f1068cac474e22fc63dd12d2
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-14 23:02:28 +0100

    Add code to create new test

--- square_matrix_map.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-# Function that squares value of all integers using map
-def square_matrix_map(matrix=[]):
-    return list(map(lambda sublist: list(map(lambda x: x**2, sublist)), matrix))
