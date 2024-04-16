6726c638c1eea87b8ef3ff56f4efe307b23f48eb
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-16 18:26:13 +0100

    Add documentation and test code

--- Python/test-driven-development/add_integer.py
+++ Python/test-driven-development/add_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-""" Adds two integers """
 def add_integer(a, b=98):
+    """Returns the integer addtion of a and b
+
+    Float arguments are typecasted to int before addition is performed
+    
+    Raises:
+        TypeError if a or b is a non-integer or non-float
+    """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     elif type(b) not in (int, float):
