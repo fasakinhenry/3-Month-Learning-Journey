a8b6441e3c6d7b12ae0479f97f7e1f37a16ded6b
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-26 11:47:16 +0100

    feat(os.js): create a code to print the free and total memory of the computer using the OS module

---
+++ Javascript/Backend/Nodejs/02-Node_module_system/os.js
@@ -0,0 +1,10 @@
+// Import the OS module from Node
+const os = require('os');
+
+// Use the OS module to get the total memory of the computer
+const totalMemory = os.totalmem();
+// Use the OS module to get the free memmory of the computer
+const freeMemory = os.freemem();
+
+// Log the total and free memory of the system to the console
+console.log('Total Memory: ' + totalMemory, '\n' + 'Free Memory: ' + freeMemory);
