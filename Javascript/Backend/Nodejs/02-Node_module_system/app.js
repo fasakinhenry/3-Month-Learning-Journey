fbca9de6797dbc9866e8fbdf64fb0f7f002cdec1
Fasakin Henry <fasakinhenry@gmail.com>
2024-05-01 10:22:10 +0100

    feat (app.js): add event listener for the logged event

--- Javascript/Backend/Nodejs/02-Node_module_system/app.js
+++ Javascript/Backend/Nodejs/02-Node_module_system/app.js
@@ -4,4 +4,12 @@ const EventEmitter = require('events');
 const Logger = require('./logger.js');
 // Create a new object from the Logger class
 const logger = new Logger();
+
+// Register a listener to show logging
+
+// Register a listener to show message logged
+logger.on('messageLogged', (eventArg) => {
+	console.log(`Listener called with an ID: ${eventArg['id']} and used the ${eventArg['url']} url`);
+});
+
 logger.log('Message');
