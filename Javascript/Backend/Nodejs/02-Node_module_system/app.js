380c4ea802789487a81d50cb70688ef0076c3a80
Fasakin Henry <fasakinhenry@gmail.com>
2024-05-01 10:03:26 +0100

    feat: add features to emit a log event to show the progress of application

--- Javascript/Backend/Nodejs/02-Node_module_system/app.js
+++ Javascript/Backend/Nodejs/02-Node_module_system/app.js
@@ -1,3 +1,5 @@
+const EventEmitter = require('events');
+
 // Import the log function from logger.js
 const logger = require('./logger.js');
 
--- Javascript/Backend/Nodejs/02-Node_module_system/events.js
+++ Javascript/Backend/Nodejs/02-Node_module_system/events.js
@@ -5,12 +5,21 @@ const EventEmitter = require('events');
 // Create an object from the event class
 const emmiter = new EventEmitter();
 
-// Listen for an event
-emmiter.on('messageLogged', function() {
-	// Log a message to the console
-	console.log('Listener called');
+// Listen for the logging event
+emmiter.addListener('logging', (eventArg) => {
+	// Show progress of application and log the message to be printed to console
+	console.log('logging', eventArg['data']);
 });
 
-// Raise an event
-emmiter.emit('messageLogged');
+// Listen for an event
+emmiter.on('messageLogged', (arg) => {
+	// Log a message to the console
+	console.log('Listener called', arg);
+});
+
+// Raise an event for logging
+emmiter.emit('logging', {data: 'message'})
+
+// Raise an event for the logged message
+emmiter.emit('messageLogged', {id: 1, url: 'http://'});
 
