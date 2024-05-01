0a978c8336c797534e2c04fc2c0d31359ddfc6de
Fasakin Henry <fasakinhenry@gmail.com>
2024-05-01 10:12:04 +0100

    feat (logger.js): Create class for events and raise events

--- Javascript/Backend/Nodejs/02-Node_module_system/app.js
+++ Javascript/Backend/Nodejs/02-Node_module_system/app.js
@@ -1,6 +1,7 @@
+// Import the ecents module as a class
 const EventEmitter = require('events');
-
-// Import the log function from logger.js
-const logger = require('./logger.js');
-
-logger('Message');
+// Import the log function from logger.js (class)
+const Logger = require('./logger.js');
+// Create a new object from the Logger class
+const logger = new Logger();
+logger.log('Message');
--- Javascript/Backend/Nodejs/02-Node_module_system/logger.js
+++ Javascript/Backend/Nodejs/02-Node_module_system/logger.js
@@ -1,12 +1,23 @@
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
+// import the events module as a class
+const EventEmitter = require('events');
 
 var url = 'http://mylogger.io/log';
-
+class Logger extends EventEmitter {
+	log(message) {
+		// Send HTTP requests
+		console.log(message);
+		
+		// Raise events
+		this.emit('logging', {data: 'message'});
+		this.emit('messageLogged', {id: 1, url: 'http://'});
+	}
+}
 function log(message) {
 	// Send HTTP reuqests
 	console.log(message)
 }
 
 // Export the log function
-module.exports = log
+module.exports = Logger
