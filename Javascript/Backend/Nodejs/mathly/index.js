aea2c8df6f74eeab8bb3014be456a9e4a55b8108
Fasakin Henry <fasakinhenry@gmail.com>
2024-05-02 23:01:02 +0100

    feat: create a new package henqsoft-lib and publish it

---
+++ Javascript/Backend/Nodejs/mathly/index.js
@@ -0,0 +1,5 @@
+// Create codes to perform math functions
+
+// Addition function
+module.exports.add = (a, b) => { return a + b}
+
---
+++ Javascript/Backend/Nodejs/test/index.js
@@ -0,0 +1,8 @@
+// Import the math's library from henqosft-maths
+const maths = require('henqsoft-math');
+
+// Use the math library to add
+var result = maths.add(1, 3);
+
+//Print the result of addition to the console
+console.log(result);
